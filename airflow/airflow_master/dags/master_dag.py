10a3e02f6270ab3379d00eae5e2678912d2e38a5
mnguyen0226 <mnguyen0226@vt.edu>
2023-08-18 14:59:44 -0400

    add not-working-yet airflow

--- airflow/airflow_master/dags/master_dag.py
+++ airflow/airflow_master/dags/master_dag.py
@@ -3,28 +3,28 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    'master_dag',
+    "master_dag",
     default_args=default_args,
-    description='Run the Master to push URLs to Redis',
+    description="Run the Master to push URLs to Redis",
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 8, 18),
     catchup=False,
 )
 
 t1 = DockerOperator(
-    task_id='master_task',
-    image='master_image',  # Replace with the name you've built the master's Docker image with.
-    api_version='auto',
+    task_id="master_task",
+    image="master_image",  # Replace with the name you've built the master's Docker image with.
+    api_version="auto",
     auto_remove=True,
     network_mode="bridge",
-    dag=dag
+    dag=dag,
 )
--- airflow/airflow_worker/dags/worker_dag.py
+++ airflow/airflow_worker/dags/worker_dag.py
@@ -5,28 +5,28 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    'worker_dag',
+    "worker_dag",
     default_args=default_args,
-    description='Run the Worker to fetch URLs from Redis and scrape',
+    description="Run the Worker to fetch URLs from Redis and scrape",
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 8, 18),
     catchup=False,
 )
 
 t1 = DockerOperator(
-    task_id='worker_task',
-    image='worker_image',  # Replace with the name you've built the worker's Docker image with.
-    api_version='auto',
+    task_id="worker_task",
+    image="worker_image",  # Replace with the name you've built the worker's Docker image with.
+    api_version="auto",
     auto_remove=True,
     network_mode="bridge",
-    dag=dag
+    dag=dag,
 )
